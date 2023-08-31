8b6b15190b647b43e8fd9c8429c9faf4ce7420d2
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2023-08-31 21:02:33 +0530

    DiningPhilosopher.go

--- DiningPhilosopher.go
+++ DiningPhilosopher.go
@@ -3,81 +3,36 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
-const (
-	numPhilosophers = 3
-	maxDiningCycles = 2
-)
-
-type Philosopher struct {
-	id                  int
-	leftFork, rightFork *sync.Mutex
-	diningCycles        int
-}
-
-type DiningTable struct {
-	philosophers []*Philosopher
-	waiter       *sync.Mutex
-}
-
-func (p *Philosopher) think() {
-	fmt.Printf("Philosopher %d is thinking\n", p.id)
-	time.Sleep(time.Second)
-}
-
-func (p *Philosopher) eat() {
-	p.leftFork.Lock()
-	p.rightFork.Lock()
-
-	fmt.Printf("Philosopher %d is eating\n", p.id)
-	time.Sleep(time.Second)
-
-	p.rightFork.Unlock()
-	p.leftFork.Unlock()
-
-	p.diningCycles++
-}
-
-func (p *Philosopher) dine(table *DiningTable) {
-	for p.diningCycles < maxDiningCycles {
-		p.think()
-
-		table.waiter.Lock()
-		p.eat()
-		table.waiter.Unlock()
-	}
-}
+type Chopstick struct{ sync.Mutex }
 
 func main() {
-	table := &DiningTable{
-		philosophers: make([]*Philosopher, numPhilosophers),
-		waiter:       &sync.Mutex{},
-	}
-
-	forks := make([]*sync.Mutex, numPhilosophers)
-	for i := 0; i < numPhilosophers; i++ {
-		forks[i] = &sync.Mutex{}
-	}
-
-	for i := 0; i < numPhilosophers; i++ {
-		table.philosophers[i] = &Philosopher{
-			id:           i + 1,
-			leftFork:     forks[i],
-			rightFork:    forks[(i+1)%numPhilosophers],
-			diningCycles: 0,
-		}
-	}
+	var numPhilosophers int
+	fmt.Print("Enter number of philosophers: ")
+	fmt.Scan(&numPhilosophers)
 
 	var wg sync.WaitGroup
-	wg.Add(numPhilosophers)
+	chopsticks := make([]Chopstick, numPhilosophers)
 
-	for _, philosopher := range table.philosophers {
-		go func(p *Philosopher) {
+	for i := 0; i < numPhilosophers; i++ {
+		wg.Add(1)
+		go func(id int) {
 			defer wg.Done()
-			p.dine(table)
-		}(philosopher)
+
+			left := id
+			right := (id + 1) % numPhilosophers
+
+			chopsticks[left].Lock()
+			chopsticks[right].Lock()
+
+			fmt.Printf("Philosopher %d is eating\n", id)
+
+			chopsticks[right].Unlock()
+			chopsticks[left].Unlock()
+
+			fmt.Printf("Philosopher %d is thinking\n", id)
+		}(i)
 	}
 
 	wg.Wait()
