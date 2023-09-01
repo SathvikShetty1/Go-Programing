ad8c6803b2317ffe564a793b6eed8625eeb24bc5
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2023-09-01 07:22:24 +0530

    LargestPalindrome.go

--- LargestPalindrome.go
+++ LargestPalindrome.go
@@ -7,29 +7,32 @@ import (
 
 func isPalindrome(n int) bool {
     str := strconv.Itoa(n)
-    for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
-        if str[i] != str[j] {
+    length := len(str)
+    for i := 0; i < length/2; i++ {
+        if str[i] != str[length-i-1] {
             return false
         }
     }
     return true
 }
 
-func largestPalindromeProduct(min, max int) (int, int, int) {
+func largestPalindromeproduct(min, max int) (int, int, int) {
     largestPalindrome := 0
-    multiplicand1, multiplicand2 := 0, 0
+    var multiplicand1, multiplicand2 int
     for i := max; i >= min; i-- {
-        for j := i; j >= min; j-- {
+        for j := max; j >= min; j-- {
             product := i * j
             if product < largestPalindrome {
                 break
             }
             if isPalindrome(product) && product > largestPalindrome {
                 largestPalindrome = product
-                multiplicand1, multiplicand2 = i, j
+                multiplicand1 = i
+                multiplicand2 = j
             }
         }
     }
+
     return largestPalindrome, multiplicand1, multiplicand2
 }
 
@@ -39,8 +42,9 @@ func main() {
     fmt.Scan(&min)
     fmt.Print("Enter the maximum value: ")
     fmt.Scan(&max)
-    
-    result, multiplicand1, multiplicand2 := largestPalindromeProduct(min, max)
-    fmt.Printf("The largest palindrome product between %d and %d is: %d\n", min, max, result)
-    fmt.Printf("The multiplicands are: %d and %d\n", multiplicand1, multiplicand2)
+
+    result, multiplicand1, multiplicand2 := largestPalindromeproduct(min, max)
+    fmt.Println("The largest palindrome product is:", result)
+    fmt.Println("The multiplicand1 is:", multiplicand1)
+    fmt.Println("The multiplicand2 is:", multiplicand2)
 }
